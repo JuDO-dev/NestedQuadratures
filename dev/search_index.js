2f6863509a46b18d4a37c21dc91df99f3ba5bc14
Documenter.jl <documenter@juliadocs.github.io>
2023-02-21 11:13:37 +0000

    build based on ae55272

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NestedQuadratures","category":"page"},{"location":"#NestedQuadratures","page":"Home","title":"NestedQuadratures","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NestedQuadratures.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NestedQuadratures]","category":"page"}]
+}
